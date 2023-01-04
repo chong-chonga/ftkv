1c8b2b57d112aff407302be0481d1564a4f07cc1
乐鑫 黄 <1612682622@qq.com>
2023-01-04 18:49:58 +0800

    storage.go由之前的将所有状态写入一个文件修改为 raft state 和 snapshot 分开存储

--- src/storage/storage.go
+++ src/storage/storage.go
@@ -11,27 +11,34 @@ import (
 )
 
 //
-// support for Raft and kvdb to save persistent
-// Raft state (log &c) and k/v server snapshots.
+// Storage is to support for Raft and kvdb to save persistent data
+// To ensure that the stored raft state and snapshot are consistent, store the raft state in a file ending in .rf,
+// the snapshot and its corresponding raft state is stored in the file at the end of .rfs.
+// If an error occurs while reading the raft state or snapshot, the program will be terminated and an error message will be reported.
+// After calling the ReadRaftState or ReadSnapshot method, if the data exists, it will be cached;
+// The cache will be refreshed when SaveRaftState or SaveSnapshot is called.
 //
-
-type Storage struct {
-	mu            sync.Mutex
-	raftStateSize int
-	raftstate     []byte
-	snapshot      []byte
-	path          string
-}
-
 const fileHeader = "RAFT"
 
-func MakeStorage(server int) *Storage {
+type Storage struct {
+	mu                   sync.Mutex
+	nextRaftStateVersion int64
+	raftState            []byte
+	snapshot             []byte
+	raftStatePath        string
+	snapshotPath         string
+}
+
+func MakeStorage(serverNum int) *Storage {
 	s := &Storage{}
-	path := "raft" + strconv.Itoa(server) + ".rf"
+	raftStatePath := "raft" + strconv.Itoa(serverNum) + ".rf"
+	snapshotPath := "raft" + strconv.Itoa(serverNum) + ".rfs"
 	if runtime.GOOS == "windows" {
-		path = os.TempDir() + "\\" + path
+		raftStatePath = os.TempDir() + "\\" + raftStatePath
+		snapshotPath = os.TempDir() + "\\" + snapshotPath
 	}
-	s.path = path
+	s.raftStatePath = raftStatePath
+	s.snapshotPath = snapshotPath
 	return s
 }
 
@@ -44,50 +51,27 @@ func clone(orig []byte) []byte {
 	return x
 }
 
-func (ps *Storage) RaftStateSize() int {
-	ps.mu.Lock()
-	defer ps.mu.Unlock()
-	return ps.raftStateSize
-}
-
-//func (ps *Storage) SaveRaftState(state []byte) {
-//	ps.mu.Lock()
-//	defer ps.mu.Unlock()
-//	ps.raftstate = clone(state)
-//}
-//
-//// Save both Raft state and K/V snapshot as a single atomic action,
-//// to help avoid them getting out of sync.
-//func (ps *Storage) SaveStateAndSnapshot(state []byte, snapshot []byte) {
-//	ps.mu.Lock()
-//	defer ps.mu.Unlock()
-//	ps.raftstate = clone(state)
-//	ps.snapshot = clone(snapshot)
-//}
-//
-//func (ps *Storage) ReadRaftState() []byte {
-//	ps.mu.Lock()
-//	defer ps.mu.Unlock()
-//	return clone(ps.raftstate)
-//}
-//
-//func (ps *Storage) ReadSnapshot() []byte {
-//	ps.mu.Lock()
-//	defer ps.mu.Unlock()
-//	return clone(ps.snapshot)
-//}
-
-func (ps *Storage) SnapshotSize() int {
-	ps.mu.Lock()
-	defer ps.mu.Unlock()
-	return len(ps.snapshot)
-}
-
 func (ps *Storage) SaveRaftState(state []byte) {
 	ps.mu.Lock()
-	snapshot := ps.snapshot
-	ps.mu.Unlock()
-	ps.SaveStateAndSnapshot(state, snapshot)
+	defer ps.mu.Unlock()
+	ps.raftState = clone(state)
+	tmpFile, err := os.CreateTemp("", "raft*.rf")
+	if err != nil {
+		log.Fatalln("create temp file for persistence failed! errorInfo: ", err)
+	}
+	writer := bufio.NewWriter(tmpFile)
+	_, err = writer.WriteString(fileHeader)
+	if err != nil {
+		log.Fatalln("error occurs during writing raft state! errorInfo: write file header failed!")
+	}
+	ps.writeRaftState(writer, state)
+	writer.Flush()
+	tmpFile.Close()
+	err = os.Rename(tmpFile.Name(), ps.raftStatePath)
+	if err != nil {
+		os.Remove(tmpFile.Name())
+		log.Fatalln("save raft state file failed! errorInfo: ", err)
+	}
 }
 
 // Save both Raft state and K/V snapshot as a single atomic action,
@@ -95,98 +79,156 @@ func (ps *Storage) SaveRaftState(state []byte) {
 func (ps *Storage) SaveStateAndSnapshot(state []byte, snapshot []byte) {
 	ps.mu.Lock()
 	defer ps.mu.Unlock()
-	ps.raftstate = clone(state)
+	ps.raftState = clone(state)
 	ps.snapshot = clone(snapshot)
-	ps.raftStateSize = len(state)
-	temp, err := os.CreateTemp("", "raft*.rf")
+	tmpFile, err := os.CreateTemp("", "raft*.rfs")
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatalln("create temp file for persistence failed! errorInfo: ", err)
 	}
-	writer := bufio.NewWriter(temp)
-	stateSize := len(state)
-	writer.WriteString(fileHeader)
-	writer.WriteString(strconv.Itoa(stateSize) + "\n")
-	writer.Write(state)
+	writer := bufio.NewWriter(tmpFile)
+	_, err = writer.WriteString(fileHeader)
+	if err != nil {
+		log.Fatalln("error occurs during writing raft state! errorInfo: write file header failed!")
+	}
+	ps.writeRaftState(writer, state)
 	if ps.snapshot != nil && len(ps.snapshot) > 0 {
 		writer.Write(ps.snapshot)
 	}
 	writer.Flush()
-	temp.Close()
+	tmpFile.Close()
 	//temp.Sync()
-	err = os.Rename(temp.Name(), ps.path)
+	err = os.Rename(tmpFile.Name(), ps.snapshotPath)
 	if err != nil {
-		os.Remove(temp.Name())
-		log.Fatalln(err)
+		os.Remove(tmpFile.Name())
+		log.Fatalln("save snapshot file failed! errorInfo: ", err)
 	}
+}
+
+func (ps *Storage) writeRaftState(writer *bufio.Writer, state []byte) {
+	writer.WriteString(strconv.FormatInt(ps.nextRaftStateVersion, 10) + "\t")
+	raftStateSize := len(state)
+	writer.WriteString(strconv.Itoa(raftStateSize) + "\t")
+	writer.Write(state)
+	ps.nextRaftStateVersion++
 }
 
 func (ps *Storage) ReadRaftState() []byte {
 	ps.mu.Lock()
 	defer ps.mu.Unlock()
-	if ps.raftstate == nil {
-		ps.read()
+	if ps.raftState == nil {
+		raftState, nextRaftStateVersion, snapshot := ps.readRFAndRFS()
+		ps.raftState = raftState
+		ps.nextRaftStateVersion = nextRaftStateVersion
+		ps.snapshot = snapshot
 	}
-	return clone(ps.raftstate)
+	return clone(ps.raftState)
 }
 
 func (ps *Storage) ReadSnapshot() []byte {
 	ps.mu.Lock()
 	defer ps.mu.Unlock()
 	if ps.snapshot == nil {
-		ps.read()
+		_, _, snapshot := ps.readRFS()
+		ps.snapshot = snapshot
 	}
 	return clone(ps.snapshot)
 }
 
-func (ps *Storage) read() {
-	file, err := os.Open(ps.path)
-	if err != nil {
-		log.Println(err)
-		return
+func (ps *Storage) readRFAndRFS() ([]byte, int64, []byte) {
+	var raftState1 []byte
+	var raftState2 []byte
+	var snapshot []byte
+	var version1 int64 = -1
+	var version2 int64 = -1
+	file, err := os.Open(ps.raftStatePath)
+	if err == nil {
+		reader := bufio.NewReader(file)
+		readFileHeader(reader)
+		raftState1, version1, _ = ps.readRaftState(reader)
+		file.Close()
 	}
-	defer file.Close()
-	reader := bufio.NewReader(file)
-	fileHeaderSize := readFileHeader(reader)
-	raftState, raftStateSize := readRaftState(reader)
-	stat, _ := file.Stat()
-	ps.raftstate = raftState
-	ps.snapshot = readSnapshot(reader, stat.Size()-fileHeaderSize-raftStateSize)
+	raftState2, version2, snapshot = ps.readRFS()
+	if version1 == -1 && version2 == -1 {
+		ps.nextRaftStateVersion = 1
+	} else if version1 > version2 {
+		return raftState1, version1, snapshot
+	}
+	return raftState2, version2, snapshot
 }
 
-func readFileHeader(reader *bufio.Reader) int64 {
+func (ps *Storage) readRFS() ([]byte, int64, []byte) {
+	file, err := os.Open(ps.snapshotPath)
+	var raftState []byte
+	var version int64 = -1
+	var snapshot []byte
+	if err == nil {
+		reader := bufio.NewReader(file)
+		readLen1 := int64(readFileHeader(reader))
+		var readLen2 int64
+		raftState, version, readLen2 = ps.readRaftState(reader)
+		var stat os.FileInfo
+		stat, err = file.Stat()
+		if err != nil {
+			log.Fatalln("error getting file information, errorInfo:", err)
+		}
+		ps.snapshot = ps.readSnapshot(reader, stat.Size()-readLen1-readLen2)
+		file.Close()
+	}
+	return raftState, version, snapshot
+}
+
+func readFileHeader(reader *bufio.Reader) int {
 	for i := range fileHeader {
 		readByte, _ := reader.ReadByte()
 		if readByte != fileHeader[i] {
-			log.Fatalf("unknown file type! expected read %c, but read %c", fileHeader[i], readByte)
+			log.Fatalln("unknown file type! please check the file or delete the file! errorInfo: file header is wrong!")
 		}
 	}
-	return int64(len(fileHeader))
+	return len(fileHeader)
 }
 
-func readRaftState(reader *bufio.Reader) ([]byte, int64) {
-	readString, err := reader.ReadString('\n')
+func (ps *Storage) readRaftState(reader *bufio.Reader) ([]byte, int64, int64) {
+	var readLen int64 = 0
+	readString, err := reader.ReadString('\t')
 	if err != nil || len(readString) < 2 {
-		log.Fatalln("expect to read number, but not exists!")
+		log.Fatalln("unknown file type! please check the file or delete the file! errorInfo: expect to read version number but not exists!")
 	}
+	readLen += int64(len(readString))
 	readString = readString[0 : len(readString)-1]
-	stateSize, err := strconv.Atoi(readString)
+	var version int64
+	version, err = strconv.ParseInt(readString, 10, 64)
 	if err != nil {
-		log.Fatalln("expect to read a number, but wrong format!")
+		log.Fatalln("unknown file type! please check the file or delete the file! errorInfo: expect to read version number but wrong format!")
+	}
+	readString, err = reader.ReadString('\t')
+	if err != nil || len(readString) < 2 {
+		log.Fatalln("unknown file type! please check the file or delete the file! errorInfo: expect to read version number but not exists!")
+	}
+	readLen += int64(len(readString))
+	readString = readString[0 : len(readString)-1]
+	var stateSize int
+	stateSize, err = strconv.Atoi(readString)
+	if err != nil {
+		log.Fatalln("unknown file type! please check the file or delete the file! errorInfo: expect to read state size but wrong format!")
 	}
 	readBytes := make([]byte, stateSize)
 	read, _ := io.ReadFull(reader, readBytes)
 	if read != stateSize {
 		log.Fatalln("raft state is not fully read!")
 	}
-	return readBytes, int64(len(readString)) + 1 + int64(read)
+	readLen += int64(stateSize)
+	return readBytes, version, readLen
 }
 
-func readSnapshot(reader *bufio.Reader, reamain int64) []byte {
-	s := make([]byte, reamain)
-	io.ReadFull(reader, s)
-	_, err := reader.ReadByte()
+func (ps *Storage) readSnapshot(reader *bufio.Reader, remain int64) []byte {
+	s := make([]byte, remain)
+	_, err := io.ReadFull(reader, s)
+	if err != nil {
+		log.Fatalln("read snapshot failed! errorInfo: ", err)
+	}
+	_, err = reader.ReadByte()
 	if err == nil {
-		log.Fatalln("snapshot is not fully read!")
+		log.Println("[warning] file is not fully read!")
 	}
 	return s
 }
