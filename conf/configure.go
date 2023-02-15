d31e22a9b66690bb63630dd9a968335a46b92ad1
乐鑫 黄 <1612682622@qq.com>
2023-02-15 20:34:54 +0800

    功能添加：添加了kvserver、raft的json配置定义，使用yaml.v3读取yml配置文件，使用embed包嵌入配置

--- conf/configure.go
+++ conf/configure.go
@@ -1,9 +1,52 @@
 package conf
 
-type KVServerConf struct {
-	Password string
+import (
+	_ "embed"
+	"gopkg.in/yaml.v3"
+)
+
+type KVServiceConf struct {
+	Me       int
+	KVServer KVServerConf `yaml:"kvserver"`
+	Raft     RaftConf     `yaml:"raft"`
 }
 
-func ReadConfFrom(confFile string) (*KVServerConf, error) {
-	return nil, nil
+type KVServerConf struct {
+	// struct fields are only marshalled/unmarshalled  if they are exported
+	// using the field name lower-cased as the default key
+	Password           string
+	Port               int    `yaml:"port"`
+	MaxRaftState       int    `yaml:"maxRaftState"`
+	SessionTimeout     int    `yaml:"sessionTimeout"`
+	SessionIdSeparator string `yaml:"sessionIdSeparator"`
+	// the content preceding the first comma is used as the key,
+	// and the following comma-separated options are used to tweak the marshalling process
+	// In addition, if the key is "-", the field is ignored.
+	// Marshal using a flow style (useful for structs, sequences and maps).
+	// 不加flow并不影响结果的正确性
+}
+
+type RaftConf struct {
+	Port            int      `yaml:"port"`
+	ServerAddresses []string `yaml:"server-addresses,flow"`
+	RandomInterval  int64    `yaml:"randomInterval"`
+	ElectionTimeout int64    `yaml:"electionTimeout"`
+	Log             struct {
+		RequestVoteEnabled     bool `yaml:"requestVoteEnabled"`
+		AppendEntryEnabled     bool `yaml:"appendEntryEnabled"`
+		InstallSnapshotEnabled bool `yaml:"installSnapshotEnabled"`
+		PersistEnabled         bool `yaml:"persistEnabled"`
+	}
+}
+
+//go:embed service.yml
+var data []byte
+
+func ReadConf() (*KVServiceConf, error) {
+	conf := &KVServiceConf{}
+	err := yaml.Unmarshal(data, conf)
+	// If one or more values cannot be decoded due to a type mismatches,
+	// decoding continues partially until the end of the YAML content,
+	// and a *yaml.TypeError is returned with details for all missed values.
+	return conf, err
 }
