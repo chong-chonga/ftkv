d34f7c1fbfc6be6f0218f97c2943782331b1ffda
乐鑫 黄 <1612682622@qq.com>
2023-05-27 23:09:19 +0800

    [Bug Fix& Optimization]：当client的请求发送给错误的group时会一直重试的问题。优化了代码结构和Client名称

--- shardkv/client.go
+++ shardkv/client.go
@@ -37,8 +37,6 @@ func readClientConfig(configData []byte) (*ClientConfig, error) {
 	return &conf.ClientConfig, err
 }
 
-const defaultTimeout = 5 * time.Second
-
 func dialShardKVClients(serverAddresses []string) []shardproto.ShardKVClient {
 	serverCount := len(serverAddresses)
 	rpcClients := make([]shardproto.ShardKVClient, serverCount)
@@ -49,34 +47,36 @@ func dialShardKVClients(serverAddresses []string) []shardproto.ShardKVClient {
 	return rpcClients
 }
 
-type UserClient struct {
+type Client struct {
 	lastLeader    int
 	timeout       time.Duration
 	logEnabled    bool
-	routerClient  *router.ServiceClient
+	routerClient  *router.ProxyClient
 	serverClients []shardproto.ShardKVClient
 }
 
-func MakeUserClient(configData []byte) (*UserClient, error) {
+const defaultTimeout = 5000
+
+func MakeClient(configData []byte) (*Client, error) {
 	clientConfig, err := readClientConfig(configData)
 	if err != nil {
 		return nil, &tool.RuntimeError{Stage: "load config", Err: err}
 	}
 	routerAddresses := clientConfig.RouterAddresses
-	routerClient, err := router.MakeServiceClient(routerAddresses)
+	routerClient, err := router.MakeProxyClient(routerAddresses)
 	if err != nil {
 		return nil, &tool.RuntimeError{Stage: "configure router client", Err: err}
-	}
-	if err != nil {
 	}
 	timeout := clientConfig.Timeout
 	if timeout < 0 {
 		return nil, errors.New("timeout must be a positive number")
+	} else if timeout == 0 {
+		log.Printf("configure shardkv client info: using default timeout=%dms", defaultTimeout)
 	}
 
 	log.Printf("router client info: routerAddresses:%v, timeout=%dms, logEnabled=%v", routerAddresses, timeout,
 		clientConfig.LogEnabled)
-	client := &UserClient{
+	client := &Client{
 		routerClient: routerClient,
 		lastLeader:   -1,
 		timeout:      time.Duration(timeout) * time.Millisecond,
@@ -85,31 +85,31 @@ func MakeUserClient(configData []byte) (*UserClient, error) {
 	return client, nil
 }
 
-func (c *UserClient) logPrintf(format string, a ...interface{}) {
+func (c *Client) logPrintf(format string, a ...interface{}) {
 	if c.logEnabled {
 		log.Printf(format, a...)
 	}
 }
 
-func (c *UserClient) getRPCClients(shard int) ([]shardproto.ShardKVClient, error) {
-	configWrapper, err := c.routerClient.QueryLatest()
+func (c *Client) queryServersFor(shard int) ([]shardproto.ShardKVClient, error) {
+	clusterConfig, err := c.routerClient.QueryLatest()
 	if err != nil {
 		return nil, err
 	}
-	groupId := configWrapper.ShardsMap[shard]
-	Servers := configWrapper.ReplicaGroups[groupId]
-	if Servers == nil || len(Servers.Servers) == 0 {
+	groupId := clusterConfig.ShardsMap[shard]
+	Servers := clusterConfig.ReplicaGroups[groupId]
+	if Servers == nil || len(Servers) == 0 {
 		return nil, errors.New("no server groups alive")
 	}
-	return dialShardKVClients(Servers.Servers), nil
+	return dialShardKVClients(Servers), nil
 }
 
-func (c *UserClient) callWithRetry(shard int, args interface{}, handleRPC func(args interface{},
+func (c *Client) callWithRetry(shard int, args interface{}, handleRPC func(args interface{},
 	rpcClient shardproto.ShardKVClient) (interface{}, bool, error, bool)) (interface{}, error) {
 	rpcClients := c.serverClients
 	var err error
 	if len(rpcClients) == 0 {
-		rpcClients, err = c.getRPCClients(shard)
+		rpcClients, err = c.queryServersFor(shard)
 		if err != nil {
 			return nil, err
 		}
@@ -118,6 +118,7 @@ func (c *UserClient) callWithRetry(shard int, args interface{}, handleRPC func(a
 	serverCount := len(rpcClients)
 	s := tool.ChooseServer(c.lastLeader, serverCount)
 	rpcClient := rpcClients[s]
+	retried := false
 	for i := 0; i < serverCount; i++ {
 		ret, ok, err, wrongGroup := handleRPC(args, rpcClient)
 		if ok {
@@ -125,13 +126,17 @@ func (c *UserClient) callWithRetry(shard int, args interface{}, handleRPC func(a
 			return ret, nil
 		}
 		if wrongGroup {
-			rpcClients, err = c.getRPCClients(shard)
+			rpcClients, err = c.queryServersFor(shard)
 			if err != nil {
 				return nil, err
 			}
+			if !retried {
+				retried = true
+				serverCount = len(rpcClients)
+				i = -1
+			}
 			c.serverClients = rpcClients
-			serverCount = len(rpcClients)
-			i = -1
+			s = 0
 		}
 		if err != nil {
 			return nil, err
@@ -146,34 +151,34 @@ func (c *UserClient) callWithRetry(shard int, args interface{}, handleRPC func(a
 	return nil, errors.New("service unavailable")
 }
 
-func (c *UserClient) handleGet(request interface{}, rpcClient shardproto.ShardKVClient) (interface{}, bool, error, bool) {
+func (c *Client) handleGet(request interface{}, rpcClient shardproto.ShardKVClient) (interface{}, bool, error, bool) {
 	getRequest := request.(*shardproto.GetRequest)
 	c.logPrintf("send get request:%v", getRequest)
-	var getReply *shardproto.GetReply
+	var reply *shardproto.GetReply
 	var err error
 	{
 		ctx, cancel := context.WithTimeout(context.Background(), c.timeout)
 		defer cancel()
-		getReply, err = rpcClient.Get(ctx, getRequest)
+		reply, err = rpcClient.Get(ctx, getRequest)
 	}
-	c.logPrintf("receive get reply:%v, err=%v", getReply, err)
+	c.logPrintf("receive get reply:%v, err=%v", reply, err)
 	if err != nil {
 		s, _ := status.FromError(err)
 		if s.Code() == codes.DeadlineExceeded {
 			return nil, false, errors.New("service unavailable"), false
 		}
 	} else {
-		if getReply.ErrCode == shardproto.ResponseCode_OK {
-			return getReply.Value, true, nil, false
+		if reply.ErrCode == shardproto.ResponseCode_OK || reply.ErrCode == shardproto.ResponseCode_KEY_NOT_EXISTS {
+			return &getReply{Value: reply.Value, Exist: reply.Exist}, true, nil, false
 		}
-		if getReply.ErrCode == shardproto.ResponseCode_WRONG_GROUP {
+		if reply.ErrCode == shardproto.ResponseCode_WRONG_GROUP {
 			return nil, false, nil, true
 		}
 	}
 	return nil, false, nil, false
 }
 
-func (c *UserClient) handleSet(request interface{}, rpcClient shardproto.ShardKVClient) (interface{}, bool, error, bool) {
+func (c *Client) handleSet(request interface{}, rpcClient shardproto.ShardKVClient) (interface{}, bool, error, bool) {
 	setRequest := request.(*shardproto.SetRequest)
 	c.logPrintf("send set request:%v", setRequest)
 	var setReply *shardproto.SetReply
@@ -200,7 +205,7 @@ func (c *UserClient) handleSet(request interface{}, rpcClient shardproto.ShardKV
 	return nil, false, nil, false
 }
 
-func (c *UserClient) handleDelete(request interface{}, rpcClient shardproto.ShardKVClient) (interface{}, bool, error,
+func (c *Client) handleDelete(request interface{}, rpcClient shardproto.ShardKVClient) (interface{}, bool, error,
 	bool) {
 	deleteRequest := request.(*shardproto.DeleteRequest)
 	c.logPrintf("send delete request:%v", deleteRequest)
@@ -228,82 +233,32 @@ func (c *UserClient) handleDelete(request interface{}, rpcClient shardproto.Shar
 	return nil, false, nil, false
 }
 
-func (c *UserClient) Get(key string) (string, error) {
-	shard := router.Key2Shard(key)
-	req := &shardproto.GetRequest{Key: key}
-	v, err := c.callWithRetry(shard, req, c.handleGet)
-	if err != nil {
-		return "", err
-	}
-	return v.(string), nil
+type getReply struct {
+	Value string
+	Exist bool
 }
 
-func (c *UserClient) Set(key string, val string) error {
+func (c *Client) Get(key string) (string, bool, error) {
+	shard := router.Key2Shard(key)
+	req := &shardproto.GetRequest{Key: key}
+	r, err := c.callWithRetry(shard, req, c.handleGet)
+	if err != nil {
+		return "", false, err
+	}
+	reply := r.(*getReply)
+	return reply.Value, reply.Exist, nil
+}
+
+func (c *Client) Set(key string, val string) error {
 	shard := router.Key2Shard(key)
 	req := &shardproto.SetRequest{Key: key, Value: val}
 	_, err := c.callWithRetry(shard, req, c.handleSet)
 	return err
 }
 
-func (c *UserClient) Delete(key string) error {
+func (c *Client) Delete(key string) error {
 	shard := router.Key2Shard(key)
 	req := &shardproto.DeleteRequest{Key: key}
 	_, err := c.callWithRetry(shard, req, c.handleDelete)
 	return err
-}
-
-type ServiceClient struct {
-	rpcClients      []shardproto.ShardKVClient
-	serverAddresses []string
-	lastLeader      int
-	timeout         time.Duration
-}
-
-func MakeServiceClient(serverAddresses []string) *ServiceClient {
-	rpcClients := dialShardKVClients(serverAddresses)
-	client := &ServiceClient{
-		rpcClients:      rpcClients,
-		serverAddresses: serverAddresses,
-		lastLeader:      -1,
-		timeout:         defaultTimeout,
-	}
-	return client
-}
-
-func (c *ServiceClient) callTransfer(rpcClient shardproto.ShardKVClient, req *shardproto.TransferRequest) (*shardproto.TransferReply, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), c.timeout)
-	defer cancel()
-	return rpcClient.Transfer(ctx, req)
-}
-
-func (c *ServiceClient) Transfer(configId int32, shards []int32, data map[string]string) error {
-	req := &shardproto.TransferRequest{
-		ConfigId: configId,
-		Shards:   shards,
-		Data:     data,
-	}
-	rpcClients := c.rpcClients
-	serverCount := len(rpcClients)
-	s := tool.ChooseServer(c.lastLeader, serverCount)
-	rpcClient := rpcClients[s]
-	var reply *shardproto.TransferReply
-	var err error
-	for i := 0; i < serverCount; i++ {
-		reply, err = c.callTransfer(rpcClient, req)
-		if err == nil {
-			if reply.ErrCode == shardproto.ResponseCode_OK {
-				return nil
-			}
-			if reply.ErrCode == shardproto.ResponseCode_CONFIG_MISMATCH {
-				time.Sleep(100 * time.Millisecond)
-			}
-		}
-		// retry
-		s++
-		if s >= serverCount {
-			s = 0
-		}
-		rpcClient = rpcClients[s]
-	}
-	return errors.New("")
 }
